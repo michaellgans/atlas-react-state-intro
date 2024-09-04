dcb4c3a604689503f4fbdb50009ca39952405da5
michaellgans <michaellgans@Victoria>
2024-09-04 14:35:35 -0500

    MLG - 2:35 PM - enables pagination

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -4,12 +4,16 @@
 import { useEffect, useState } from "react";
 import { TableRow } from "./components/TableRow";
 
+// Page Size
+const PAGE_SIZE = 5;
+
 export default function SchoolCatalog() {
   // Hooks
   const [courses, setCourses] = useState([]);
   const [filter, setFilter] = useState("");
   const [sort, setSort] = useState("trimester")
   const [direction, setDirection] = useState("asc");
+  const [page, setPage] = useState(1);
 
   // Fetching data from a local JSON
   useEffect(() => {
@@ -31,10 +35,10 @@ export default function SchoolCatalog() {
       return (Number(a.trimester) - Number(b.trimester)) * (direction === "desc" ? -1 : 1);
     }
     if (sort === "courseNumber") {
-      return (a[sort].localeCompare(b.courseNumber)) * (direction === "desc" ? -1 : 1);
+      return (a.courseNumber.localeCompare(b.courseNumber)) * (direction === "desc" ? -1 : 1);
     }
     if (sort === "courseName") {
-      return (a[sort].localeCompare(b.courseName)) * (direction === "desc" ? -1 : 1);
+      return (a.courseName.localeCompare(b.courseName)) * (direction === "desc" ? -1 : 1);
     }
     if (sort === "semesterCredits") {
       return (Number(a.semesterCredits) - Number(b.semesterCredits)) * (direction === "desc" ? -1 : 1);
@@ -51,6 +55,16 @@ export default function SchoolCatalog() {
     setSort(field);
     setDirection(sortOrder);
   }
+
+  // Reset current page to 1 for Filter and Sort
+  useEffect(() => {
+    setPage(1);
+  }, [filter, sort]);
+
+  // Pagination
+  const currentPage = sortedData.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
+  const hasMore = sortedData.length > page * PAGE_SIZE;
+  const hasLess = page > 1;
 
   return (
     <div className="school-catalog">
@@ -75,14 +89,14 @@ export default function SchoolCatalog() {
         {/* Table Body */}
         <tbody>
         {/* Generates TableRow Components per JSON Object */}
-          {sortedData.map((course, index) => (
+          {currentPage.map((course, index) => (
             <TableRow key={index} {...course} />
           ))}
         </tbody>
       </table>
       <div className="pagination">
-        <button>Previous</button>
-        <button>Next</button>
+        <button disabled={!hasLess} onClick={() => setPage(page - 1)}>Previous</button>
+        <button disabled={!hasMore} onClick={() => setPage(page + 1)}>Next</button>
       </div>
     </div>
   );
