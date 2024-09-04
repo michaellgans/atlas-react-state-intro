ab9836c5cb4e24355de88439cea351d9a23fa9c9
michaellgans <michaellgans@Victoria>
2024-09-04 16:45:20 -0500

    MLG - 4:45 PM - implements number of classes count total

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,21 @@
 import logo from "./assets/logo.png";
+import { useContext, useState, useEffect } from "react";
+import { AppContext } from "./ContextProvider.jsx";
 
 export default function Header() {
+  // Hooks
+  const { enrolledCourses } = useContext(AppContext);
+  const [numCourses, setNumCourses] = useState(0);
+
+  // Count Courses
+  useEffect(() => {
+    setNumCourses(enrolledCourses.length);
+  }, [enrolledCourses]);
+
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
+      <div className="enrollment">Classes Enrolled: {numCourses}</div>
     </div>
   );
 }
