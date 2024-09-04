95f89147d17fadc733719bb0b13e07df5875da89
michaellgans <michaellgans@Victoria>
2024-09-04 13:50:55 -0500

    MLG - 1:50 PM - implements sortation

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -2,16 +2,17 @@
 
 // Imports
 import { useEffect, useState } from "react";
-import courseData from "../public/api/courses.json"
 import { TableRow } from "./components/TableRow";
 
 export default function SchoolCatalog() {
-  // Hook
+  // Hooks
   const [courses, setCourses] = useState([]);
   const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("trimester")
+  const [direction, setDirection] = useState("asc");
 
+  // Fetching data from a local JSON
   useEffect(() => {
-    // Fetching data from a local JSON
     fetch("/api/courses.json")
       // Converts data into a JavaScript object
       .then((response) => response.json())
@@ -21,9 +22,35 @@ export default function SchoolCatalog() {
       });
   }, []);
 
-  console.log({courses});
+  // Filter data based on input typed
+  const filteredData  = courses.filter((item) => item.courseNumber.toLowerCase().startsWith(filter) || item.courseName.toLowerCase().startsWith(filter));
 
-  const filteredData  = courses.filter((item) => item.courseNumber.startsWith(filter) || item.courseName.startsWith(filter));
+  // Sort data based on header clicked
+  const sortedData = filteredData.sort((a, b) => {
+    if (sort === "trimester") {
+      return (Number(a.trimester) - Number(b.trimester)) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "courseNumber") {
+      return (a[sort].localeCompare(b.courseNumber)) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "courseName") {
+      return (a[sort].localeCompare(b.courseName)) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "semesterCredits") {
+      return (Number(a.semesterCredits) - Number(b.semesterCredits)) * (direction === "desc" ? -1 : 1);
+    }
+    if (sort === "totalClockHours") {
+      return (Number(a.totalClockHours) - Number(b.totalClockHours)) * (direction === "desc" ? -1 : 1);
+    }
+    return 0;
+  });
+
+  // Sort direction
+  const handleSortingChange = (field) => {
+    const sortOrder = sort === field && direction === "asc" ? "desc" : "asc";
+    setSort(field);
+    setDirection(sortOrder);
+  }
 
   return (
     <div className="school-catalog">
@@ -31,24 +58,24 @@ export default function SchoolCatalog() {
       <input 
         type="text" 
         placeholder="Search Course Number or Name"
-        onChange={(event) => setFilter(event.target.value)}
+        onChange={(event) => setFilter(event.target.value.toLowerCase())}
       />
       <table>
         {/* Table Head */}
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
+            <th onClick={() => handleSortingChange("trimester")}>Trimester</th>
+            <th onClick={() => handleSortingChange("courseNumber")}>Course Number</th>
+            <th onClick={() => handleSortingChange("courseName")}>Courses Name</th>
+            <th onClick={() => handleSortingChange("semesterCredits")}>Semester Credits</th>
+            <th onClick={() => handleSortingChange("totalClockHours")}>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
         {/* Table Body */}
         <tbody>
         {/* Generates TableRow Components per JSON Object */}
-          {filteredData.map((course, index) => (
+          {sortedData.map((course, index) => (
             <TableRow key={index} {...course} />
           ))}
         </tbody>
